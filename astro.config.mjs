708168f0f4a39aa48eaa8e831a3e78a22c26898a
Zhen Ting, Neo <neozt99@gmail.com>
2024-02-16 19:55:30 +0800

    Added base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://neozt.github.io',
-  // base: '/my-repo',
+  base: '/astro-test',
 });
